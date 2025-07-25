870a6df324e002302639dd40b1062b07fe75275a
Oliveira-Dan <oliveira.busi.ss@gmail.com>
2025-07-24 22:37:40 -0300

    add Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "Olá, programador";
+        System.out.println(message);
+        System.out.println("Continue estudando " + args[0] + "!");
+        System.out.println( "Sua plataforma de estudos é: "+ args[1]);
+    }
+}
